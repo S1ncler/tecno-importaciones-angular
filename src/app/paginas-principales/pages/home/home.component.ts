dd85c3467fbcb987849dbcd40e42c546554d5565
AlejandraGomez26 <alejagomez.026@gmail.com>
2023-06-11 02:18:35 -0500

    efecto mouse en las marcas

--- src/app/paginas-principales/pages/home/home.component.ts
+++ src/app/paginas-principales/pages/home/home.component.ts
@@ -6,5 +6,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  public mostrarCuadro1: boolean = false;
+  public mostrarCuadro2: boolean = false;
+  public mostrarCuadro3: boolean = false;
+  public mostrarCuadro4: boolean = false;
+  public mostrarCuadro5: boolean = false;
+  public mostrarCuadro6: boolean = false;
+  public mostrarCuadro7: boolean = false;
+  public mostrarCuadro8: boolean = false;
 
+
+  public toggleCuadro(cuadro: string): void {
+    switch (cuadro) {
+      case 'cuadro1':
+        this.mostrarCuadro1 = !this.mostrarCuadro1;
+        break;
+      case 'cuadro2':
+        this.mostrarCuadro2 = !this.mostrarCuadro2;
+        break;
+      case 'cuadro3':
+        this.mostrarCuadro3 = !this.mostrarCuadro3;
+        break;
+      case 'cuadro4':
+        this.mostrarCuadro4 = !this.mostrarCuadro4;
+        break;
+      case 'cuadro5':
+        this.mostrarCuadro5 = !this.mostrarCuadro5;
+        break;
+      case 'cuadro6':
+        this.mostrarCuadro6 = !this.mostrarCuadro6;
+        break;
+      case 'cuadro7':
+        this.mostrarCuadro7 = !this.mostrarCuadro7;
+        break;
+      case 'cuadro8':
+        this.mostrarCuadro8 = !this.mostrarCuadro8;
+        break;
+      default:
+        break;
+    }
+  }
+  
 }
