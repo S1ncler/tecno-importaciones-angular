a8eef559823b79faeb1f46587a26ee61c3af8269
SEBASTIAN OSORIO OROTIZ <110748370+sebas20osorio@users.noreply.github.com>
2023-06-27 13:44:44 -0500

    sebas

--- src/app/comercio/pages/detalles-producto/detalles-producto.component.ts
+++ src/app/comercio/pages/detalles-producto/detalles-producto.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-detalles-producto',
   templateUrl: './detalles-producto.component.html',
   styleUrls: ['./detalles-producto.component.css']
 })
-export class DetallesProductoComponent {
 
+
+export class DetallesProductoComponent {
+ 
+  
+  
 }
 
